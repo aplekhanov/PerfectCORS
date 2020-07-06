bbe88b2aa26a6eefe3bdab2a23ab42a3f2c09805
Alexey Plekhanov <alexey.plekhanov@gmail.com>
2020-07-05 23:19:12 -0700

    Initial Commit

---
+++ PerfectCORS/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  PerfectCORS
+//
+//  Created by Jack Sparrow on 7/5/20.
+//  Copyright © 2020 Alexey Plekhanov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
